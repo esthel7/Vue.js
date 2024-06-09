b2c9dbed5f7facc02cf0009e27ee83632864f047
esthel <esthel9026@naver.com>
2024-06-10 01:16:54 +0900

    feat: defineProps, toRefs로 인자 사용하기

--- src/components/atoms/layout/BasicLayout.vue
+++ src/components/atoms/layout/BasicLayout.vue
@@ -4,7 +4,16 @@
   </div>
 </template>
 
-<script setup lant="ts" />
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+interface Props {
+  theme?: string;
+}
+
+const props = defineProps<Props>();
+const { theme } = toRefs(props);
+</script>
 
 <style lang="scss" scoped>
 .basic-layout {
