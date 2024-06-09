a1c343015416898017ba2d12b7eb9c1c7a2a96d7
esthel <esthel9026@naver.com>
2024-06-10 02:26:53 +0900

    feat: v-for

--- src/components/organisms/Navigation.vue
+++ src/components/organisms/Navigation.vue
@@ -2,22 +2,85 @@
   <BasicLayout :theme="'blue'">
     <div class="navigation">
       <div class="navigation_sub-menu">
-        <div class="navigation_sub-menu_button-box"></div>
+        <div class="navigation_sub-menu_button-box">
+          <BasicButton v-for="item in topMenu" :key="item.label" :data="item" />
+        </div>
       </div>
       <div class="navigation_sub-menu">
         <span class="navigation_sub-menu_title">Recruitment</span>
-        <div class="navigation_sub-menu_button-box"></div>
+        <div class="navigation_sub-menu_button-box">
+          <BasicButton
+            v-for="item in middleMenu"
+            :key="item.label"
+            :data="item"
+          />
+        </div>
       </div>
       <div class="navigation_sub-menu">
         <span class="navigation_sub-menu_title">Organization</span>
-        <div class="navigation_sub-menu_button-box"></div>
+        <div class="navigation_sub-menu_button-box">
+          <BasicButton
+            v-for="item in bottomMenu"
+            :key="item.label"
+            :data="item"
+          />
+        </div>
       </div>
     </div>
   </BasicLayout>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import BasicLayout from '@components/atoms/layout/BasicLayout.vue';
+import BasicButton from '@components/atoms/button/BasicButton.vue';
+
+const menu = [
+  {
+    icon: 'Dashboard',
+    label: 'Dashboard',
+  },
+  {
+    icon: 'Indox',
+    label: 'Indox',
+  },
+  {
+    icon: 'Calendar',
+    label: 'Calendar',
+  },
+  {
+    icon: 'Jobs',
+    label: 'Jobs',
+  },
+  {
+    icon: 'Reference',
+    label: 'Reference',
+  },
+  {
+    icon: 'Document',
+    label: 'Document',
+  },
+  {
+    icon: 'CareerSite',
+    label: 'CareerSite',
+  },
+  {
+    icon: 'Employee',
+    label: 'Employee',
+  },
+  {
+    icon: 'Structure',
+    label: 'Structure',
+  },
+  {
+    icon: 'Settings',
+    label: 'Settings',
+  },
+];
+
+const topMenu = computed(() => menu.slice(0, 3));
+const middleMenu = computed(() => menu.slice(3, 7));
+const bottomMenu = computed(() => menu.slice(7, menu.length));
 </script>
 
 <style lang="scss" scoped>
