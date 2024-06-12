92cef2b003b481a9d8f3a9a5a093f20452fa9c92
esthel <esthel9026@naver.com>
2024-06-12 20:03:36 +0900

    design: css 작업

--- src/components/organisms/Navigation.vue
+++ src/components/organisms/Navigation.vue
@@ -88,9 +88,10 @@ const bottomMenu = computed(() => menu.slice(7, menu.length));
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: center;
+  justify-content: flex-start;
   width: 100%;
-  gap: 40px;
+  height: calc(70vh - 40px);
+  gap: 14px;
 
   &_sub-menu {
     display: flex;
