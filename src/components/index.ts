80983b32cc8a1b5c03046c0895f092cc0687c7df
esthel <esthel9026@naver.com>
2024-06-22 01:49:48 +0900

    add: basic layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as SquareLayout } from './SquareLayout.vue';
+export { default as BasicLayout } from './BasicLayout.vue';
 export { default as Navigation } from './Navigation.vue';
 export { default as BasicButton } from './BasicButton.vue';
 export { default as CandidateBox } from './CandidateBox.vue';
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,48 +1,9 @@
 <template>
-  <div class="page">
-    <SquareLayout>
-      <div class="page_container">
-        <section class="page_container_navigation">
-          <Navigation />
-        </section>
-        <section class="page_container_candidate">
-          <CandidateBox />
-        </section>
-      </div>
-    </SquareLayout>
-  </div>
+  <BasicLayout>
+    <CandidateBox />
+  </BasicLayout>
 </template>
 
 <script setup lang="ts">
-import { CandidateBox, Navigation, SquareLayout } from '@components';
+import { BasicLayout, CandidateBox } from '@components';
 </script>
-
-<style lang="scss" scoped>
-.page {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100vh;
-  background-color: $color-black-900;
-
-  &_container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
-    width: 95vw;
-    height: 95vh;
-
-    &_navigation {
-      width: 20%;
-      height: 100%;
-    }
-
-    &_candidate {
-      width: 80%;
-      height: 100%;
-    }
-  }
-}
-</style>
