8f08207cddd7642acb94d7f426413b304f6f0234
esthel <esthel9026@naver.com>
2024-06-22 04:38:11 +0900

    design: skill badge

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export { default as Navigation } from './Navigation.vue';
 export { default as BasicButton } from './BasicButton.vue';
 export { default as CandidateBox } from './CandidateBox.vue';
 export { default as CandidateList } from './CandidateList.vue';
+export { default as Badge } from './Badge.vue';
 export { default as BarChart } from './BarChart.vue';
 export { default as Pagination } from './Pagination.vue';
 export { default as SortJob } from './SortJob.vue';
